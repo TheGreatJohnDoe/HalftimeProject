5df00b1ad2ad8da046d2e3f65b58c50c406352dc
Omer Angi <omerangi10@gmail.com>
2021-04-29 14:57:24 +0300

    Changed the config file and created the DBHelper class to connect with the DB

---
+++ RequestsMaster/RequestsMaster/Utility/DBHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace RequestsMaster.Utility
+{
+    public static class DBHelper
+    {
+        public static string ConnectionValue(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
