f559c909668b3d8120a1197c179d7a6e136858a7
Omer Angi <omerangi10@gmail.com>
2021-05-05 14:06:48 +0300

    Added requests controller

---
+++ RequestsMaster/RequestsMaster/Controllers/NewRequestsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RequestsMaster.Controllers
+{
+    public class NewRequestsController : Controller
+    {
+        // GET: NewRequests
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteUser()
+        {
+            return View();
+        }
+
+        public ActionResult EditUser()
+        {
+            return View();
+        }
+
+        public ActionResult RequestAdmin()
+        {
+            return View();
+        }
+    }
+}
